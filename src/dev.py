f59139f28a363916b082e308a303b1016923a004
dabane-ghassan <dabane.ghassan@gmail.com>
2021-01-19 21:58:56 +0100

    factoring suffix array and bwt transform

--- src/dev.py
+++ src/dev.py
@@ -5,43 +5,34 @@ Created on Tue Jan 19 15:09:30 2021
 @author: KugelBlitZZZ
 """
 import filemanager as fm
-import numpy as np
+from typing import List
 
 file = fm.FileManager("../data/test_seq.txt")
 seq = file.read()
 
+"""first
 rot = seq *2
 yo = sorted(rot[i:i+len(seq)] for i in range(len(seq)))
 
-seq 
+seq
 ''.join([y[-1] for y in yo])
-
-
-t = sorted([(seq[i:], i) for i in range(len(seq))])
-t
-
-bw = []
-for si in map(lambda x: x[1], t):
-    if si == 0:
-        bw.append('$')
-    else:
-        bw.append(seq[si-1])
-        
-''.join(bw)
-
-
 """
-def suffixArray(s):
-    satups = sorted([(s[i:], i) for i in range(len(s))])
-    return map(lambda x: x[1], satups)
 
-def bwtViaSa(t):
-    # Given T, returns BWT(T) by way of the suffix array
-    bw = []
-    for si in suffixArray(t):
-        if si == 0:
-            bw.append('$')
+"""second"""
+
+def suffix_array(sequence: str) -> List[tuple]:
+    
+    sequence += '$'
+    return sorted([(sequence[c:], c) for c in range(len(sequence))])
+
+def bwt_transform(sequence: str) -> str:
+   
+    bwt = []
+    for suff in suffix_array(sequence):
+        i = suff[1]
+        if i == 0:
+            bwt.append('$')
         else:
-            bw.append(t[si-1])
-    return ''.join(bw) # return string-ized version of list bw
-"""
+            bwt.append(sequence[i - 1])
+    return ''.join(bwt)
+
--- src/filemanager.py
+++ src/filemanager.py
@@ -7,19 +7,19 @@ A script to read and write DNA sequences (or a list of characters) to a file.
 
 class FileManager:
     
-    def __init__(self, path):
+    def __init__(self: object, path: str) -> None:
         self.path = path
         
-    def __str__(self):
+    def __str__(self: object) -> str:
         return "Hey there! my only mission is to read and write files"
     
-    def __repr__(self):
+    def __repr__(self: object) -> str:
         return "FileManager(%s)" % self.path
         
-    def read(self):
+    def read(self: object) -> str:
         with open(self.path, 'r') as f:
             return f.readline()
         
-    def write(self, out, content):
+    def write(self: object, out: str, content: str) -> None:
         with open(out, 'w') as f:
             f.writelines(content)
