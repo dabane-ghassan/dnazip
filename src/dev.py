3bf5db8574481521b6f19310106a0524f9148374
dabane-ghassan <dabane.ghassan@gmail.com>
2021-02-06 20:19:08 +0100

    finalizing bwt naive approach

--- src/dev.py
+++ src/dev.py
@@ -5,17 +5,68 @@ Created on Tue Jan 19 15:09:30 2021
 @author: KugelBlitZZZ
 """
 import filemanager as fm
-from typing import List, Tuple
+from typing import List
 
 file = fm.FileManager("../data/test_seq.txt")
 seq = file.read()
 
-"""second"""
 
+
+def string_rotations(seq: str) -> List[str]:
+  
+    seq += '$'
+    double_seq = seq * 2
+    all_rotations = []
+    
+    for i in range(0, len(seq), 1):
+        all_rotations.append(double_seq[i:i+len(seq)])
+        
+    return all_rotations
+
+def bw_matrix(rotations: List[str]) -> List[str]:
+
+    sorted_rotations = sorted(rotations)
+    return sorted_rotations
+
+def bw_transform(matrix: List[str]) -> str:
+    
+    last_column = []
+    for l in matrix:
+        last_char = l[-1]
+        last_column.append(last_char)
+    
+    transformed_seq = ''.join(last_column)
+    return transformed_seq
+
+
+
+seq
+all_rots = string_rotations(seq)
+all_rots
+mat = bw_matrix(all_rots)
+mat
+t = bw_transform(mat)
+t
+ls = []
+for i in range(len(t)):
+    ls.append('')
+
+for n in range(len(t)):
+    for i in range(len(t)):
+        ls[i] = t[i] + ls[i]
+    ls.sort()
+ls[4]
+mat
+seq
+
+
+"""
+
+################## suffix array to construct BWT
 def suffix_array(sequence: str) -> List[Tuple[str, int]]:
-    """O(n^2log(n)) sort nlogn and finding is n,
+    '''O(n^2log(n)) sort nlogn and finding is n,
     maybe multikey quicksort to get that O(n^2) or try something better x(
-    """
+    '''
 
     sequence += '$'
     
@@ -31,9 +82,11 @@ def bwt_transform(sequence: str) -> str:
         else:
             bwt.append(sequence[i - 1])
     return ''.join(bwt)
+####################################################
 
+############################# Reverse BWT using b ranks and lf mapping
 def rankBwt(bw):
-    ''' Given BWT string bw, return parallel list of B-ranks.  Also
+    '''Given BWT string bw, return parallel list of B-ranks.  Also
         returns tots: map from character to # times it appears. '''
     tots = dict()
     ranks = []
@@ -66,15 +119,6 @@ def reverseBwt(bw):
         # jump to row that starts with c of same rank
         rowi = first[c][0] + ranks[rowi]
     return t
-
-yo = bwt_transform(seq)
-
-reverseBwt(yo)
-seq
-"""first
-rot = seq *2
-yo = sorted(rot[i:i+len(seq)] for i in range(len(seq)))
-
-seq
-''.join([y[-1] for y in yo])
+#################################################################
 """
+
