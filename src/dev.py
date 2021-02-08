49884aea088adf10978804dac4b890ec381b3d04
dabane-ghassan <dabane.ghassan@gmail.com>
2021-02-08 18:20:52 +0100

    factoring burros-wheeler

--- src/dev.py
+++ src/dev.py
@@ -4,61 +4,100 @@ Created on Tue Jan 19 15:09:30 2021
 
 @author: KugelBlitZZZ
 """
+import os
 import filemanager as fm
 from typing import List
 
+class BurrosWheeler:
+    """A class to represent a Burros-Wheeler transform.
+
+    Attributes
+    ----------
+
+    """
+    def __init__(self, file_path):
+
+        self.sequence_path = file_path
+        self.sequence = fm.FileManager(self.sequence_path).read()
+        self.bw_transform = None
+
+    def __str__(self):
+        
+        return "Hello! I'm a BurrosWheeler class instance"
+
+    def __repr__(self):
+        
+        return "BurrosWheeler('%s', '%s')" %(self.sequence_path,
+                                             self.bw_transform)
+
+
+    @staticmethod
+    def string_rotations(seq: str) -> List[str]:
+      
+        seq += '$'
+        double_seq = seq * 2
+        all_rotations = []
+        
+        for i in range(0, len(seq), 1):
+            all_rotations.append(double_seq[i:i+len(seq)])
+            
+        return all_rotations
+
+    @staticmethod
+    def construct_bwm(rotations: List[str]) -> List[str]:
+    
+        sorted_rotations = sorted(rotations)
+        return sorted_rotations
+
+    @staticmethod
+    def encode_bwt(matrix: List[str]) -> str:
+        
+        last_column = []
+        for l in matrix:
+            last_char = l[-1]
+            last_column.append(last_char)
+        
+        transformed_seq = ''.join(last_column)
+        return transformed_seq
+
+    @staticmethod
+    def reconstruct_bwm(bwt: str) -> List[str]:
+        
+        bwm = []
+        # first loop to create seeds for lines O(n)
+        for l in range(0, len(bwt), 1): 
+            bwm.append('')
+    
+        for n in range(len(bwt)):
+            for i in range(len(bwt)):
+                bwm[i] = bwt[i] + bwm[i]
+            bwm.sort()
+        return bwm
+
+    @staticmethod    
+    def decode_bwt(matrix: List[str]) -> str:
+        
+        seq = ""
+        for l in matrix:
+            if l[-1] == "$":
+                seq += l
+        
+        return seq[:-1]
+
+os.chdir("C:/Users/33750/Documents/GitHub/dnazip/src")
 file = fm.FileManager("../data/test_seq.txt")
 seq = file.read()
-
-
-
-def string_rotations(seq: str) -> List[str]:
-  
-    seq += '$'
-    double_seq = seq * 2
-    all_rotations = []
-    
-    for i in range(0, len(seq), 1):
-        all_rotations.append(double_seq[i:i+len(seq)])
-        
-    return all_rotations
-
-def bw_matrix(rotations: List[str]) -> List[str]:
-
-    sorted_rotations = sorted(rotations)
-    return sorted_rotations
-
-def bw_transform(matrix: List[str]) -> str:
-    
-    last_column = []
-    for l in matrix:
-        last_char = l[-1]
-        last_column.append(last_char)
-    
-    transformed_seq = ''.join(last_column)
-    return transformed_seq
-
-
-
 seq
-all_rots = string_rotations(seq)
+all_rots = BurrosWheeler.string_rotations(seq)
 all_rots
-mat = bw_matrix(all_rots)
+mat = BurrosWheeler.construct_bwm(all_rots)
 mat
-t = bw_transform(mat)
+t = BurrosWheeler.encode_bwt(mat)
 t
-ls = []
-for i in range(len(t)):
-    ls.append('')
-
-for n in range(len(t)):
-    for i in range(len(t)):
-        ls[i] = t[i] + ls[i]
-    ls.sort()
-ls[4]
-mat
-seq
-
+remat = BurrosWheeler.reconstruct_bwm(t)
+remat  
+deseq = BurrosWheeler.decode_bwt(remat)
+deseq == seq
 
 """
 
