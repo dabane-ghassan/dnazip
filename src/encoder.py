d8fc3dfcd4b18d56d1e7844fa8a8ee157c7449cd
dabane-ghassan <dabane.ghassan@gmail.com>
2021-02-28 10:19:32 +0100

    adding methods decoder

--- src/encoder.py
+++ src/encoder.py
@@ -6,20 +6,21 @@ Created on Sun Feb 21 22:19:48 2021
 @author: Ghassan Dabane
 """
 from __future__ import absolute_import
+from typing import Iterator, List
 from sequence import Sequence
 from burros_wheeler import BurrosWheeler
 from huffman import HuffmanTree
 
 class Encoder:
-    
+
     def __init__(self: object, path: str) -> None:
-        
+
         self.path = path[:-4]
         self.seq = Sequence(path)
-        self.bwt_output = self.path + '_bwt.txt'
-        self.encoded_output = self.path + '_compressed.txt'
-   
-    def encode_bw(self: object) -> None:
+        self.bwt_encode = self.path + '_bwt.txt'
+        self.huffman_encode = self.path + '_compressed.txt'
+
+    def encode_bw(self: object) -> Iterator[List[str]]:
         
         all_rots = BurrosWheeler.string_rotations(self.seq.read())
         yield all_rots
@@ -28,12 +29,12 @@ class Encoder:
         yield bwm
         
         tf = BurrosWheeler.encode_bwt(bwm)
-        self.seq.write(self.bwt_output, tf)
+        self.seq.write(self.bwt_encode, tf)
         yield tf
-        
-    def encode_huffman(self: object) -> None:
+ 
+    def encode_huffman(self: object) -> Iterator[str]:
 
-        tree = HuffmanTree(self.bwt)
+        tree = HuffmanTree(self.seq.read())
         tree.get_codings(tree.root)
         binary = tree.seq_to_binstr()
         yield binary
@@ -41,12 +42,15 @@ class Encoder:
         unicode = HuffmanTree.binstr_to_unicode(binary)
         header = tree.codes_to_header()
         compressed =  header + unicode
-        self.seq.write_bytes(self.encoded_output, compressed)
+        self.seq.write_bytes(self.huffman_encode, compressed)
         yield unicode
         
+    def full_protocol(self: object) -> None:
+        pass
+
 
 pfft = Encoder("../data/random.txt")
 
-ts = pfft.encode()
+ts = pfft.encode_bw()
 
 next(ts)
