47670634960a04f270bb40f652f6cd2117176b9d
dabane-ghassan <dabane.ghassan@gmail.com>
2021-03-02 16:34:14 +0100

    refactoring encoder classes

--- dnazip/encoder.py
+++ dnazip/encoder.py
@@ -6,44 +6,65 @@ Created on Sun Feb 21 22:19:48 2021
 @author: Ghassan Dabane
 """
 from __future__ import absolute_import
-from typing import Iterator, List
 from sequence import Sequence
 from burros_wheeler import BurrosWheeler
 from huffman import HuffmanTree
 
-class Encoder:
+class BWEncoder:
 
     def __init__(self: object, path: str) -> None:
 
         self.path = path[:-4]
         self.seq = Sequence(path)
-        self.bwt_encode = Sequence(self.path + '_bwt.txt')
-        self.huffman_encode = Sequence(self.path + '_compressed.txt')
+        self.bwt_output = self.path + '_bwt.txt'
+        self.rotations = None
+        self.bwm = None
+        self.bwt = None
 
-    def encode_bw(self: object) -> Iterator[List[str]]:
+    def encode(self: object) -> None:
         
-        all_rots = BurrosWheeler.string_rotations(self.seq.read())
+        self.rotations = BurrosWheeler.string_rotations(self.seq.read())
+        self.bwm = BurrosWheeler.construct_bwm(self.rotations)
+        self.bwt = BurrosWheeler.encode_bwt(self.bwm)
+        Sequence(self.bwt_output).write(self.bwt)
 
-        
-        bwm = BurrosWheeler.construct_bwm(all_rots)
+class HuffEncoder:
+    
+    def __init__(self: object, path: str) -> None:
 
-        
-        tf = BurrosWheeler.encode_bwt(bwm)
-        self.bwt_encode.write(tf)
+        self.path = path[:-4]
+        self.seq = Sequence(path)
+        self.huff_output = self.path + '_compressed.txt'
+        self.binary = None
+        self.compressed = None
 
+    def encode(self: object) -> None:
 
-    def encode_huffman(self: object) -> Iterator[str]:
-
-        tree = HuffmanTree(self.bwt_encode.read())
+        tree = HuffmanTree(self.seq.read())
         tree.get_codings(tree.root)
-        binary = tree.seq_to_binstr()
-
-
-        unicode = HuffmanTree.binstr_to_unicode(binary)
+        self.binary = tree.seq_to_binstr()
+        unicode = HuffmanTree.binstr_to_unicode(self.binary)
         header = tree.codes_to_header()
-        compressed =  header + unicode
-        self.huffman_encode.write_bytes(compressed)
+        self.compressed =  header + unicode
+        Sequence(self.huff_output).write_bytes(self.compressed)
 
-  
-    def full_protocol(self: object) -> None:
-        pass
+class FullEncoder:
+
+    def __init__(self: object, path: str) -> None:
+
+        self.path = path
+        
+        
+    def full_zip(self: object) -> None:
+
+        self.bw_encoder = BWEncoder(self.path)
+        self.bw_encoder.encode()
+        
+        self.huff_encoder = HuffEncoder(self.bw_encoder.bwt_output)
+        self.huff_encoder.encode()
+
+
+yo = FullEncoder("../data/randseq2kb.txt")
+yo.full_zip()
+
+yo.huff_encoder.compressed
